f7a403d5200c222dc4eee592136fa5589db3940c
ac-devs19 <ac.devs.07192002@gmail.com>
2025-11-16 18:17:55 +0800

    remove csrf in web layout

--- resources/views/layouts/web.blade.php
+++ resources/views/layouts/web.blade.php
@@ -4,13 +4,11 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta name="csrf-token" content="{{ csrf_token() }}">
   <meta name="google-site-verification" content="X3jWLb8LBSj3SI1rvMOHqDCFdPB3XhL62KXwWJaOR-c" />
   <link rel="icon" type="image/x-icon" href="{{ asset('images/icon.png') }}">
+  @yield('meta')
 
   <title>{{ config('app.name') }}</title>
-
-  @yield('meta')
 
   @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
