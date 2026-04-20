acfdb080c1a4baed8a10c0afe8735b3d969281fb
ac-devs19 <ac.devs.07192002@gmail.com>
2026-04-20 14:29:45 +0800

    modify news api for mobile

--- app/Http/Controllers/Mobile/NewsController.php
+++ app/Http/Controllers/Mobile/NewsController.php
@@ -8,19 +8,27 @@ use Illuminate\Http\Request;
 
 class NewsController extends Controller
 {
-    public function news()
+    public function news(Request $request)
     {
-        $news = News::with('sdg')
+        $search = $request->input('search');
+
+        $news = News::select('id', 'title', 'image', 'date')
+            ->when($search, function ($query, $search) {
+                $query->where('title', 'like', "%{$search}%");
+            })
             ->orderByDesc('date')
-            ->get();
+            ->paginate(10);
 
         return response()->json($news);
     }
 
     public function article($id)
     {
-        $article = News::where('id', $id)
-            ->with('sdg')
+        $article = News::select('id', 'title', 'description', 'image', 'date')
+            ->where('id', $id)
+            ->with('sdg', function ($query) {
+                $query->select('image');
+            })
             ->first();
 
         return response()->json($article);
